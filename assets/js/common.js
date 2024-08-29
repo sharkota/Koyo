ddc5a5ab3754b675524a7fd16d4a98a0164a6e17
Sharkota <97995086+sharkota@users.noreply.github.com>
2024-08-29 16:42:59 -0500

    Neocities Fixes

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,6 @@
 // general use functions
 playsound = function (name = String, vol = 1, loop = false) {
-    let audio = new Audio('/assets/audio/' + name);
+    let audio = new Audio('https://kota.share.zrok.io/cdn/audio/' + name);
     audio.volume = vol
     audio.loop = loop
     audio.play();
