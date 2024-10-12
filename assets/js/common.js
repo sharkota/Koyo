867354c3bf3b62ba238dfb15522f6efb031854aa
Sharkota <97995086+sharkota@users.noreply.github.com>
2024-10-12 03:42:50 -0500

    Audio Moved to Nekoweb

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,6 @@
 // general use functions
 playsound = function (name = String, vol = 1, loop = false) {
-    let audio = new Audio('https://kota.share.zrok.io/cdn/audio/' + name);
+    let audio = new Audio('https://dakota.nekoweb.org/' + name);
     audio.volume = vol
     audio.loop = loop
     audio.play();
